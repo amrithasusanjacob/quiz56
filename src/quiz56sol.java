6bb426af0d953e0414119ff09011606299d3333a
753813 <753813@123-PC07.student.Tor.Loc>
2019-06-04 16:13:39 -0400

    R1.One person is studying

--- src/quiz56sol.java
+++ src/quiz56sol.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 
 public class quiz56sol {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner obj = new Scanner(System.in);
+		System.out.println("Enter the name");
+		String name=obj.nextLine();
+       String r= studying(name);
+       System.out.println(r);
 	}
-
+   public static String studying(String name)
+   {
+	  String s= name+" is studying";
+	  return s;
+   }
 }
